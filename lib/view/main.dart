1c7fe592d1291d20d67f82d95190f630b8527cf0
zunera-adil <137143591+zunera-adil@users.noreply.github.com>
2024-06-26 23:59:46 +0200

    set firebase

--- lib/view/main.dart
+++ lib/view/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:happyplant/view/welcome_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized(
+  );
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: 'key',
+        projectId: 'myapp', appId: 'appid', messagingSenderId: 'zunera@adil',
+      )
+  );
   runApp(const HappyPlant());
 }
 
